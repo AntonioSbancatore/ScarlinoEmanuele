7e5857f2b2631c92ec25ee1914ae45b3b17dcb9f
4c <4c@INF01>
2022-05-04 12:01:56 +0200

    vers. 1.0

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"ScarlinoEmanueleClass","l":"main(String[])","url":"main-java.lang.String:A-"},{"p":"<Unnamed>","c":"ScarlinoEmanueleClass","l":"ScarlinoEmanueleClass()"},{"p":"<Unnamed>","c":"ScarlinoEmanueleClass","l":"scarlinoMet(int)"}]
---
+++ src/ScarlinoEmanueleClass.java
@@ -0,0 +1,26 @@
+
+/**
+ * 
+ * @author 4c
+ * @version 1.0
+ */
+public class ScarlinoEmanueleClass {
+	/**
+	 * 
+	 * @param f
+	 * @return
+	 */
+	public int scarlinoMet(int f) {
+		int r=1;
+		for (int i=f; i<0; i++) {
+			r=r*i;
+		}
+		return r;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
